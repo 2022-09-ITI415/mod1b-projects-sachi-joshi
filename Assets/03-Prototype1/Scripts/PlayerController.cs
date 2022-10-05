af82b371af13ef4fcefc08ed21151b80c783951f
sachi-joshi <76460499+sachi-joshi@users.noreply.github.com>
2022-10-05 18:20:27 -0400

    fixed camera position

--- Assets/03-Prototype1/Scripts/PlayerController.cs
+++ Assets/03-Prototype1/Scripts/PlayerController.cs
@@ -1,81 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PlayerControler : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
-    //Create public variables for player speed, and for the Text UI
-    //public float speed = 0;
-    //public TextMeshProUGUI countText;
-   // public GameObject winTextObject;
+    public float speed = 20f;
+    public Text countText;
+    public Text winText;
 
-    private Rigidbody rb;
-    //private int count;
-    private float movementX;
-    private float movementY;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    int m_Count;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //Assign Rigidbody component
-        rb = GetComponent<Rigidbody>();
-        //count = 0;
-
-        //SetCountText();
-
-       // winTextObject.SetActive(false);
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Count = 0;
+        setCountText();
+        winText.text = "";
     }
 
-   void OnMove(InputValue movementValue)
+    void FixedUpdate() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+
+        m_Rigidbody.AddForce(m_Movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
     {
-        
-       Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            m_Count++;
+            setCountText();
+        }
     }
 
-    //void SetCountText()
-    //{
-        //countText.text = "Count: " + count.ToString();
-   // }
-    //}
+    void OnCollisionEnter (Collision coll) {
+        if (coll.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 
-    void FixedUpdate()
+    void setCountText()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        countText.text = "Count: " + m_Count.ToString();
+        if (m_Count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
-
-    //void OnTriggerEnter(Collider other)
-    //{
-        //if(other.gameObject.CompareTag("PickUp"))
-        //{
-            //other.gameObject.SetActive(false);
-            //count = count + 1;
-
-            //SetCountText();
-       // }
-    //}
-
-//void OnMove(InputValue value)
-//{
-    //ector2 v = value.Get<Vector2>();
-
-    //movementX = v.x;
-    //movementY = v.y;
-//}
-
-//void SetCountText()
-    //{
-       // countText.text = "Count: " + count.ToString();
-
-        //if (count >= 12)
-        //{
-            //winTextObject.SetActive(true);
-       // }
-   ////}
 }
---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,10 @@
+
+//using UnityEngine;
+
+//public class PlayerCollision : MonoBehaviour {
+
+  //  void OnCollisionEnter() {
+    //    n
+    //}
+//}
+
